5a2ee67b063c8f854bb8ce538ba61a2f6a7978f7
patrykkobiela <57020326+patrykkobiela@users.noreply.github.com>
2020-01-04 13:53:05 +0100

    Update wisielec.c

--- wisielec.c
+++ wisielec.c
@@ -1,1 +1,22 @@
-#include "man.h"
+#include "man.h";
+
+int main()       
+{
+    int a=0,score=0; 
+    int i,j,f;        
+    int enter=0;      
+    char ch;          
+    char h[6]={ '_','_','_','_','_' };      
+    char word;
+    char words3[][3]=
+	    { "top",
+	     "rat",
+	     "bat",
+	     "cat",
+	     "man",
+	     "hat",
+	     "hut",
+	     "cut",
+	     "bit",
+	     "pen" };
+}
